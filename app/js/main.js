73c9d7dbbe3897ab3719ae95d1170a02251560a8
Max Hewett <mjhewett01@gmail.com>
2015-08-03 13:03:38 -0400

    styling

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,10 @@
 $('.button').on('click', function (){
   $('#p1').removeClass('p1');
   $('#p1').addClass('change');
+  $('#buttonTwo').removeClass('b2');
+  $('#buttonTwo').addClass('display');
+  $('display').fadeIn("slow");
+
 
 
   console.log('clicked');
@@ -10,4 +14,6 @@ $('.button').on('click', function (){
 $('.b2').on('click', function() {
   $('#p1').removeClass('change').fadeIn();
   $('#p1').addClass('p1');
+  $('#buttonTwo').removeClass('display');
+  $('#buttonTwo').addClass('b2');
 });
