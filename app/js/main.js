1f15585da33af87618ab988b6e73b6f83d566d7c
Max Hewett <mjhewett01@gmail.com>
2015-08-02 20:11:04 -0400

    added jquery functions. Still figuring out color scheme.

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
 
+$('.button').on('click', function (){
+  $('#p1').removeClass('p1');
+  $('#p1').addClass('change');
+
+
+  console.log('clicked');
+});
+
+$('.b2').on('click', function() {
+  $('#p1').removeClass('change').fadeIn();
+  $('#p1').addClass('p1');
+});
