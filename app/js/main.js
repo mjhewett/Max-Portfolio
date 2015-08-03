104551739cdc423e2ffb1d66e03ed93389ad63ff
Max Hewett <mjhewett01@gmail.com>
2015-08-03 18:14:28 -0400

    added some buttons/fun stuff. Still need to add linkedin and github and final project.

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,39 @@
 
+console.log('Hi there!');
+
+
 $('.button').on('click', function (){
+
   $('#p1').removeClass('p1');
-  $('#p1').addClass('change');
   $('#buttonTwo').removeClass('b2');
-  $('#buttonTwo').addClass('display');
-  $('display').fadeIn("slow");
-
-
-
-  console.log('clicked');
+  $('#p1').addClass('change');
+  $('#buttonTwo').addClass('display').fadeIn("slow");
 });
+
+
 
 $('.b2').on('click', function() {
+
   $('#p1').removeClass('change').fadeIn();
+  $('#buttonTwo').removeClass('display').fadeOut("slow");
   $('#p1').addClass('p1');
-  $('#buttonTwo').removeClass('display');
   $('#buttonTwo').addClass('b2');
 });
+
+
+$('.funnyBox').on('click', function(){
+  console.log('DELETING ALL FILES!');
+  $('#bod').addClass('alt');
+  $('#p1').removeClass('p1');
+  $('#p1').addClass('alternative');
+  $('#newButton').removeClass('newButton');
+  $('#newButton').addClass('appear');
+});
+
+
+$('.newButton').on('click', function(){
+  $('#bod').removeClass('alt');
+  $('#p1').removeClass('alternative');
+  $('#p1').addClass('p1');
+});
+
